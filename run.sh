204e03f5630474f48a78dc938b4e1c5ad0d38c28
Jonathan <git@jonathansm.dev>
2023-06-12 14:32:35 -0500

    feat: add shell script to set up the environment and run the bot
    Made it easier to run the bot from a cron job by having it run a shell
    script which completes the required setup and runs the Python script.

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+source venv/bin/activate
+venv/bin/python3 main.py -c config.json
