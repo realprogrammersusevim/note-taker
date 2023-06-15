6d991a9d9e0620477acf4e6b180d97160337a558
Jonathan <git@jonathansm.dev>
2023-06-14 19:52:17 -0500

    feat: add metadata command
    You can now have the bot publish a metadata event to show the bot's
    profile metadata.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 import math
 import time
 
-from nostr.event import EncryptedDirectMessage
+from nostr.event import EncryptedDirectMessage, Event, EventKind
 from nostr.filter import Filter, Filters
 from nostr.key import PrivateKey
 from nostr.message_type import ClientMessageType
@@ -14,6 +14,9 @@ from nostr.relay_manager import RelayManager
 
 parser = argparse.ArgumentParser(description="Note Taker")
 parser.add_argument("-c", "--config", help="Path to config file")
+parser.add_argument(
+    "-m", "--metadata", action="store_true", help="Publish metadata about the bot"
+)
 args = parser.parse_args()
 
 config = json.load(open(args.config))
@@ -35,70 +38,80 @@ relay_manager = RelayManager()
 for relay in config["relays"]:
     relay_manager.add_relay(relay)
 
-if config["should_dm"]:
-    private_key = PrivateKey().from_nsec(config["private_key"])
-    start_dm = EncryptedDirectMessage(
-        recipient_pubkey=config["public_key"], cleartext_content="Taking notes"
-    )
-    private_key.sign_event(start_dm)
-    relay_manager.publish_event(start_dm)
+private_key = PrivateKey().from_nsec(config["private_key"])
 
-filter = Filters([Filter(authors=[config["public_key"]], since=config["last_run"])])
-sub_id = "ilovenostr"
-request = [ClientMessageType.REQUEST, sub_id]
-request.extend(filter.to_json_array())
+if args.metadata:
+    metadata = json.dumps(config["metadata"])
+    logging.debug(f"Metadata: {metadata}")
+    metadata_event = Event(metadata, kind=EventKind.SET_METADATA)
+    private_key.sign_event(metadata_event)
+    logging.debug(f"Metadata event: {metadata_event.to_message()}")
+    relay_manager.publish_event(metadata_event)
+    logging.info("Metadata published")
+else:
+    if config["should_dm"]:
+        start_dm = EncryptedDirectMessage(
+            recipient_pubkey=config["public_key"], cleartext_content="Taking notes"
+        )
+        private_key.sign_event(start_dm)
+        relay_manager.publish_event(start_dm)
 
-relay_manager.add_subscription(sub_id, filter)
-relay_manager.open_connections()
-time.sleep(1)
-logging.info("Connections open")
+    filter = Filters([Filter(authors=[config["public_key"]], since=config["last_run"])])
+    sub_id = "ilovenostr"
+    request = [ClientMessageType.REQUEST, sub_id]
+    request.extend(filter.to_json_array())
 
-message = json.dumps(request)
-relay_manager.publish_message(message)
-time.sleep(1)
-logging.info("Message published")
+    relay_manager.add_subscription(sub_id, filter)
+    relay_manager.open_connections()
+    time.sleep(1)
+    logging.info("Connections open")
 
-event_list = []
+    message = json.dumps(request)
+    relay_manager.publish_message(message)
+    time.sleep(1)
+    logging.info("Message published")
 
-while relay_manager.message_pool.has_events():
-    event_msg = relay_manager.message_pool.get_event()
-    current_event = event_msg.event
+    event_list = []
 
-    if current_event.verify():
-        event_list.append(event_msg.event)
+    while relay_manager.message_pool.has_events():
+        event_msg = relay_manager.message_pool.get_event()
+        current_event = event_msg.event
 
-logging.info("Events received")
+        if current_event.verify():
+            event_list.append(event_msg.event)
 
-deduped_events = []
+    logging.info("Events received")
 
-for event in event_list:
-    if event not in deduped_events:
-        deduped_events.append(event)
+    deduped_events = []
 
-deduped_events.sort(key=lambda x: x.created_at)
+    for event in event_list:
+        if event not in deduped_events:
+            deduped_events.append(event)
 
-stringified = [event.to_message() for event in deduped_events]
+    deduped_events.sort(key=lambda x: x.created_at)
 
-logging.debug(f"New events: {stringified}")
-logging.info("Events deduped, writing to file")
+    stringified = [event.to_message() for event in deduped_events]
 
-with open(config["output"], "a") as f:
-    f.write("\n".join(stringified))
+    logging.debug(f"New events: {stringified}")
+    logging.info("Events deduped, writing to file")
 
-last_run = math.floor(time.time())
-logging.debug(f"New last run: {last_run}")
-new_config = config
-new_config["last_run"] = last_run
-with open(args.config, "w") as f:
-    json.dump(new_config, f, indent=4)
+    with open(config["output"], "a") as f:
+        f.write("\n".join(stringified))
 
-if config["should_dm"]:
-    end_dm = EncryptedDirectMessage(
-        recipient_pubkey=config["public_key"],
-        cleartext_content=f"Finished taking {len(stringified)} new notes",
-    )
-    private_key.sign_event(end_dm)
-    relay_manager.publish_event(end_dm)
+    last_run = math.floor(time.time())
+    logging.debug(f"New last run: {last_run}")
+    new_config = config
+    new_config["last_run"] = last_run
+    with open(args.config, "w") as f:
+        json.dump(new_config, f, indent=2)
+
+    if config["should_dm"]:
+        end_dm = EncryptedDirectMessage(
+            recipient_pubkey=config["public_key"],
+            cleartext_content=f"Finished taking {len(stringified)} new notes",
+        )
+        private_key.sign_event(end_dm)
+        relay_manager.publish_event(end_dm)
 
 logging.info("Closing connections")
 relay_manager.close_connections()
