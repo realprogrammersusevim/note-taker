881e1fa7395c297e847225c98c16cf3eda626c23
Jonathan <git@jonathansm.dev>
2023-06-17 12:58:00 -0500

    fix(run.sh): continue running the script if no venv
    Even if the the user has not set up a virtual environment and is running
    system Python the script should still continue to work.

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd "$SCRIPT_DIR"
-source venv/bin/activate
-venv/bin/python3 main.py -c config.json
+cd "$SCRIPT_DIR" || exit
+source venv/bin/activate ||
+python3 main.py -c config.json
